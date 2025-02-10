3a8eaf5a9b08595b2ddb34bbf45f448091c7aad2
Syed Muhammad Aun Hasan Naqavi <122987113+MuhammadAun-gif@users.noreply.github.com>
2025-02-11 01:29:36 +0500

    Create shape.cpp

---
+++ C++/shape.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string characterName = "John";
+    int characterAge = 45;
+
+    cout << "Hello " << characterName << endl;
+    cout << "He was " << characterAge << " Years old" << endl;
+}
